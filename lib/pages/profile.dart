46317d6d7aacb7f4f8c3a81b6f513a403bce4932
mohamed Baher <bahermohamad13@gmail.com>
2021-12-14 17:48:31 +0200

    added profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,9 @@
+import 'package:final_project/models/post.dart';
+import 'package:final_project/providers/posts_provider.dart';
+import 'package:final_project/providers/user_provider.dart';
+import 'package:final_project/widgets/post_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Profile extends StatefulWidget {
   Profile({Key? key}) : super(key: key);
@@ -9,9 +14,58 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
     return Center(
-        child: Text("Profile Page"),
-      );
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          CircleAvatar(
+            backgroundImage: NetworkImage(user!.image!),
+            radius: 50,
+          ),
+          const SizedBox(height: 20),
+          Text(
+            '${user.email}',
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            '${user.username}',
+            style: const TextStyle(fontSize: 20),
+          ),
+          Divider(thickness: 1),
+          Consumer<PostsProvider>(builder: (context, data, child) {
+            List<Post> posts = [];
+            data.posts.forEach((element) {
+              if (user.username == element.creatorName) {
+                posts.add(element);
+              }
+            });
+
+            if (posts.isEmpty) {
+              return Container(
+                margin: const EdgeInsetsDirectional.all(50),
+                child: const CircularProgressIndicator(),
+              );
+            } else {
+              return Expanded(
+                child: ListView.builder(
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) {
+                      return PostCard(index: index, post: posts[index]);
+                    }),
+              );
+            }
+          }),
+        ],
+      ),
+    );
   }
 }
