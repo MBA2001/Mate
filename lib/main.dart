d530bd54fc6efc81d83746077f90824a126acc43
mohamed Baher <bahermohamad13@gmail.com>
2022-01-20 20:02:46 +0200

    cleaning main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:final_project/pages/account_settings.dart';
-import 'package:final_project/pages/comment_page.dart';
-import 'package:final_project/pages/home.dart';
 import 'package:final_project/pages/login.dart';
 import 'package:final_project/pages/other_user_profile.dart';
-import 'package:final_project/pages/profile.dart';
 import 'package:final_project/pages/register.dart';
-import 'package:final_project/pages/search.dart';
 import 'package:final_project/providers/posts_provider.dart';
 import 'package:final_project/providers/user_provider.dart';
 import 'package:final_project/wrapper.dart';
@@ -14,7 +10,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'pages/settings.dart';
 
-import 'models/user.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
